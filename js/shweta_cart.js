ac1e37edea97110ffec7ff416ea2c623439fdf0b
Shweta01-Beladiya <shweta.kalathiyainfotech@gmail.com>
2024-09-19 18:03:27 +0530

    shweta19-9

--- js/shweta_cart.js
+++ js/shweta_cart.js
@@ -5,32 +5,7 @@ document.getElementById('add_item_btn').addEventListener('click', function (even
     document.getElementById('filled-cart').style.display = 'block';
     document.getElementById('empty-cart').style.display = 'none';
 })
- 
-// add items in array
 
-// const cardData = [
-//     {
-//       name: "Masala Dosa",
-//       image: "../shweta_img/img4.png",
-//       vegIcon: "../shweta_img/veg.svg",
-//       description: "Lorem ipsum dolor sit amet consectetur...",
-//       category: "South Indian",
-//       rating: 4.5,
-//       price: "$120",
-//       quantity: 1,
-//     },
-//     {
-//       title: "Masala Dosa",
-//       image: "../shweta_img/img4.png",
-//       vegIcon: "../shweta_img/veg.svg",
-//       description: "Lorem ipsum dolor sit amet consectetur...",
-//       category: "South Indian",
-//       rating: 4.5,
-//       price: "$120",
-//       quantity: 1,
-//     },
-   
-//   ];
 // Cart data array
 const cartData = [
     {
@@ -62,7 +37,7 @@ function renderLargeScreenCart() {
 
     cartData.forEach((item, index) => {
         const itemHtml = `
-            <div class="col-xl-3 col-md-4">
+            <div class="col-xl-3 col-md-4 col-12">
                 <div class="card sb_card1" style="width: 18rem;">
                     <img src="${item.image}" class="card-img-top" alt="${item.name}" style="position: relative;">
                     <img src="${item.vegIcon}" alt="Veg icon" style="position: absolute; top: 5%; right: 10%;">
@@ -83,10 +58,10 @@ function renderLargeScreenCart() {
                                     <p style="font-size: 14px;">Customize</p>
                                 </div>
                                 <div class="sb_qty_btn">
-                                ${item.quantity === 1 
-                                    ? `<span><i class="fa-solid fa-trash-can" onclick="deleteItem(${index})" style="color: #000 !important;"></i></span>`
-                                    : `<span><i class="fa-solid fa-minus" onclick="updateQuantity(${index}, -1)"></i></span>`
-                                }
+                                ${item.quantity === 1
+                ? `<span><i class="fa-solid fa-trash-can" onclick="deleteItem(${index})" style="color: #000 !important;"></i></span>`
+                : `<span><i class="fa-solid fa-minus" onclick="updateQuantity(${index}, -1)"></i></span>`
+            }
                                     <span>${item.quantity}</span>
                                     <span><i class="fa-solid fa-plus" onclick="updateQuantity(${index}, 1)"></i></span>
                                 </div>
@@ -100,47 +75,6 @@ function renderLargeScreenCart() {
     });
 }
 
-// Function to render small screen cart items
-function renderSmallScreenCart() {
-    const container = document.getElementById('small-screen-cart');
-    container.innerHTML = '';
-
-    cartData.forEach((item, index) => {
-        const itemHtml = `
-            <div class="col-12 d-flex justify-content-between p-2">
-                <div>
-                    <img src="${item.image}" alt="${item.name}" width="70px">
-                </div>
-                <div>
-                    <h6>${item.name}</h6>
-                    <small>${item.category}</small>
-                    <p>$${item.price}</p>
-                </div>
-                <div>
-                    <small style="color: #FB7514;">Customize</small>
-                    <div class="sb_qty_btn">
-                         ${item.quantity === 1 
-                            ? `<span><i class="fa-solid fa-trash-can" onclick="deleteItem(${index})" style="color: #000 !important;"></i></span>`
-                            : `<span><i class="fa-solid fa-minus" onclick="updateQuantity(${index}, -1)"></i></span>`
-                        }
-                        <span>${item.quantity}</span>
-                        <span><i class="fa-solid fa-plus" onclick="updateQuantity(${index}, 1)"></i></span>
-                    </div>
-                </div>
-            </div>
-            <hr>
-        `;
-        container.innerHTML += itemHtml;
-    });
-
-    container.innerHTML += `
-        <div style="display: flex; justify-content: space-between; align-items: center; padding: 10px;">
-            <p class="m-0">Add More Items</p>
-            <img src="../shweta_img/plus.svg" alt="">
-        </div>
-    `;
-}
-
 // Function to update quantity and re-render cart
 function updateQuantity(index, change) {
     cartData[index].quantity = Math.max(1, cartData[index].quantity + change);
@@ -149,7 +83,7 @@ function updateQuantity(index, change) {
 }
 
 function deleteItem(index) {
-    cartData.splice(index,1);
+    cartData.splice(index, 1);
     renderCart();
     updateTotals();
 }
@@ -161,16 +95,38 @@ function updateTotals() {
 
     document.getElementById('total-items').textContent = `Total ${totalItems} Items`;
     document.getElementById('total-price').textContent = `$${totalPrice}`;
-    document.getElementById('total-items-sm').textContent = `Total (${totalItems} items)`;
-    document.getElementById('total-price-sm').textContent = `$${totalPrice}`;
-}
 
+}
 // Function to render the entire cart
 function renderCart() {
     renderLargeScreenCart();
-    renderSmallScreenCart();
     updateTotals();
 }
-
 // Initial render
 renderCart();
+
+
+//Model Open & Close Order summary, Review, thank you
+document.addEventListener('DOMContentLoaded', function () {
+    const orderSummaryModal = new bootstrap.Modal(document.getElementById('orderSummaryModal'));
+    const reviewModal = new bootstrap.Modal(document.getElementById('reviewModal'));
+    const thankYouModal = new bootstrap.Modal(document.getElementById('thankYouModal'));
+
+    // Open Order Summary Modal when "Place Order" is clicked
+    document.getElementById('placeOrderBtn').addEventListener('click', function () {
+        orderSummaryModal.show();
+    });
+
+    // Open Review Modal when "Add Review" is clicked
+    document.getElementById('addReviewBtn').addEventListener('click', function () {
+        orderSummaryModal.hide();
+        reviewModal.show();
+    });
+
+    // Open Thank You Modal when "Submit" is clicked
+    document.getElementById('submitReviewBtn').addEventListener('click', function () {
+        reviewModal.hide();
+        thankYouModal.show();
+    });
+});
+
